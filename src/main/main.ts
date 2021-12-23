adb1b93dc8c4f90ffad816702211ced1342b023c
jo-ho <joshuagjho@hotmail.com>
2021-12-22 22:40:31 -0800

    App now starts maximized; Add repos.json to gitignore

--- src/main/main.ts
+++ src/main/main.ts
@@ -193,6 +193,8 @@ const createWindow = async () => {
     if (process.env.START_MINIMIZED) {
       mainWindow.minimize();
     } else {
+      mainWindow.maximize();
+
       mainWindow.show();
     }
   });
